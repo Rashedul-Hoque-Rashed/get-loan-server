5203ad6f6ae942cda777d996345c779339c6100e
Rashed <rashedulhoqueceo@gmail.com>
2023-12-25 22:40:15 +0600

    create get news by id

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/newses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await newsCollections.findOne(query);
+            res.send(result)
+        })
+
 
 
 
